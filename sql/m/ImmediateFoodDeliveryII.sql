a8b923d9b2ae95683dcb42a7950606144bb7d1ed
Yaroslav G Hamuha <yhamuha@gmail.com>
2025-09-27 23:22:10 -0700

    sql. m. immediate-food-delivery-ii

--- sql/m/ImmediateFoodDeliveryII.sql
+++ sql/m/ImmediateFoodDeliveryII.sql
@@ -1,6 +1,6 @@
 -- https://leetcode.com/problems/immediate-food-delivery-ii/
 SELECT
-    ROUND(
+    ROUND (
         100.0 * SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) / COUNT(*),
         2
     ) AS immediate_percentage
